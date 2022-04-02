99e08e857968459336edbf980e79b2791f233914
Dima <dima.trotsiuk322@gmail.com>
2022-04-02 15:43:20 +0200

    Recorded basic styles for body and menu. Developed the page "CСклад Діми і Влада"

--- stickers_site/storage/views.py
+++ stickers_site/storage/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import StickersMain, StickersDima
+from .models import StickersMain, StickersDima, StickersVlad
+
 
 # Create your views here.
 
@@ -10,21 +11,26 @@ def storage_all(request):
 
 def storage_dima_vlad(request):
     storage_dima = StickersDima.objects.select_related('stickers_main').all()
+    storage_vlad = StickersVlad.objects.select_related('stickers_main').all()
 
-    for pack in storage_dima:
+    formatting_quantity(storage_dima)
+    formatting_quantity(storage_vlad)
+
+    context = {
+        "storages": [{'storage': storage_dima, 'owner_storage': 'Склад Діми'},
+                     {'storage': storage_vlad, 'owner_storage': 'Склад Влада'}],
+    }
+
+    return render(request, 'storage/storage_dima_vlad.html', context)
+
+
+def formatting_quantity(storage):
+    for pack in storage:
         quantity = pack.quantity
         quantity_in_pack = pack.stickers_main.quantity_in_pack
-        print(quantity, quantity_in_pack)
 
         if quantity % quantity_in_pack == 0:
             pack.quantity = int(quantity/quantity_in_pack)
         else:
             pack.quantity = quantity/quantity_in_pack
 
-    storage_vlad = StickersDima.objects.all()
-    context = {
-        "storage_dima": storage_dima,
-        "storage_vlad": storage_vlad
-    }
-
-    return render(request, 'storage/storage_dima_vlad.html', context)
