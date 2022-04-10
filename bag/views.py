ae68630cd441d220df1f0f0fd486d8be203ce072
Dima <dima.trotsiuk322@gmail.com>
2022-04-10 18:51:15 +0200

    Style edits

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
 from bag.models import BagProduct
 from order_management.views import check_bag
@@ -6,14 +6,21 @@ from order_management.views import check_bag
 
 def bag(request):
     if request.user.is_authenticated:
-        bag_user = check_bag(request.user)
+        if request.method == "GET":
+            remove_item_id = request.GET.get('remove_item')
+            if remove_item_id:
+                remove_item = get_object_or_404(BagProduct, id=remove_item_id)
+                remove_item.delete()
+                return redirect('bag')
+            bag_user = check_bag(request.user)
 
-        storages = BagProduct.objects.select_related('product').filter(bag=bag_user)
+            storages = BagProduct.objects.select_related('product').filter(bag=bag_user)
 
-        context = {
-            'products_in_bag': storages,
-            'bag': bag_user,
-        }
-        return render(request, "bag/bag.html", context)
+            context = {
+                'products_in_bag': storages,
+                'bag': bag_user,
+            }
+            return render(request, "bag/bag.html", context)
+
     else:
         return render(request, "access_is_blocked.html")
