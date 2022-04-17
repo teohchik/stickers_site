2dd4727a1bdb9dc925528085aecfae7d041103ee
Dima <dima.trotsiuk322@gmail.com>
2022-04-17 09:45:12 +0200

    Created form EditOrder

--- order_management/forms.py
+++ order_management/forms.py
@@ -1,34 +1,23 @@
-# from django.core.exceptions import ValidationError
-# from django.forms import ModelForm, TextInput
-# from storage.models import StickersStorage
-#
-#
-# class GetQuantityDimaForm(ModelForm):
-#     class Meta:
-#         model = StickersStorage
-#         fields = ['quantity_dima']
-#
-#         widgets = {
-#             "quantity": TextInput(attrs={
-#                 'class': 'form-quantity',
-#                 'placeholder': '25',
-#             }),
-#         }
-#
-#     def clean_quantity_dima(self):
-#         if self.cleaned_data['quantity_dima'] == 1:
-#             raise ValidationError('Кастомна валідація')
-#         return self.cleaned_data['quantity_dima']
-#
-#
-# class GetQuantityVladForm(ModelForm):
-#     class Meta:
-#         model = StickersStorage
-#         fields = ['quantity_vlad']
-#
-#         widgets = {
-#             "quantity": TextInput(attrs={
-#                 'class': 'form-quantity',
-#                 'placeholder': '25',
-#             }),
-#         }
+from django.core.exceptions import ValidationError
+from django.forms import ModelForm, TextInput, ChoiceField, Select
+
+from order_management.models import Order
+
+
+class EditOrder(ModelForm):
+    class Meta:
+        model = Order
+        fields = ['status']
+
+        widgets = {
+            'status': Select(attrs={
+                'class': 'status'
+            }),
+        }
+
+    # def clean_quantity_dima(self):
+    #     if self.cleaned_data['quantity_dima'] == 1:
+    #         raise ValidationError('Кастомна валідація')
+    #     return self.cleaned_data['quantity_dima']
+
+
