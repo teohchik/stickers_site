b7b3b48f4da9705efc519f204704ccb8c5c8b346
Dima <dima.trotsiuk322@gmail.com>
2022-04-23 15:52:35 +0200

    Added level permission

--- api/bag_api/views.py
+++ api/bag_api/views.py
@@ -1,4 +1,8 @@
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
 
 from api.bag_api.serializers import BagProductsSerializer, BagSerializer
 from bag.models import BagProduct, Bag
@@ -7,14 +11,22 @@ from bag.models import BagProduct, Bag
 class BagProductsApiView(generics.ListAPIView):
     queryset = BagProduct.objects.all()
     serializer_class = BagProductsSerializer
+    permission_classes = (IsAdminUser,)
 
 
 class BagProductApiView(generics.RetrieveUpdateDestroyAPIView):
     queryset = BagProduct.objects.all()
     serializer_class = BagProductsSerializer
+    permission_classes = (IsAdminUser,)
 
 
 class BagApiView(generics.RetrieveUpdateAPIView):
     queryset = Bag.objects.all().prefetch_related('bag_products')
-
     serializer_class = BagSerializer
+    permission_classes = (IsAdminUser,)
+
+
+@api_view(['GET'])
+def create_order(request, pk):
+    print(request.user)
+    return Response(status=HTTP_200_OK)
--- api/storage_api/views.py
+++ api/storage_api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAdminUser
 
 from api.storage_api.serializers import StorageQuantitySerializer, ProductsInfoSerializer
 from storage.models import StickersStorage, StickersMain
@@ -7,23 +8,28 @@ from storage.models import StickersStorage, StickersMain
 class ProductsQuantityApiView(generics.ListAPIView):
     queryset = StickersStorage.objects.all()
     serializer_class = StorageQuantitySerializer
+    permission_classes = (IsAdminUser,)
 
 
 class ProductsInfoApiView(generics.ListAPIView):
     queryset = StickersMain.objects.filter(is_published=True).prefetch_related('storage_stickers')
     serializer_class = ProductsInfoSerializer
+    permission_classes = (IsAdminUser,)
 
 
 class ProductQusntityUpdateApiView(generics.RetrieveUpdateAPIView):
     queryset = StickersStorage.objects.all()
     serializer_class = StorageQuantitySerializer
+    permission_classes = (IsAdminUser,)
 
 
 class ProductQuantityApiView(generics.RetrieveAPIView):
     queryset = StickersStorage.objects.all()
     serializer_class = StorageQuantitySerializer
+    permission_classes = (IsAdminUser,)
 
 
 class ProductInfoApiView(generics.RetrieveAPIView):
     queryset = StickersMain.objects.filter(is_published=True)
     serializer_class = ProductsInfoSerializer
+    permission_classes = (IsAdminUser,)
